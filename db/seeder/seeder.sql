e1134d2d444466d4b4a9e74a204085742a68f79c
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2024-11-22 00:58:06 +0700

    seeder: banners & services

---
+++ db/seeder/seeder.sql
@@ -0,0 +1,23 @@
+INSERT INTO banners (banner_name, banner_image, description)
+VALUES
+  ('Banner 1', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211008/banner_sample_ial2fj.png', 'Lerem Ipsum Dolor sit amet'),
+  ('Banner 2', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211008/banner_sample_ial2fj.png', 'Lerem Ipsum Dolor sit amet'),
+  ('Banner 3', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211008/banner_sample_ial2fj.png', 'Lerem Ipsum Dolor sit amet'),
+  ('Banner 4', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211008/banner_sample_ial2fj.png', 'Lerem Ipsum Dolor sit amet'),
+  ('Banner 5', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211008/banner_sample_ial2fj.png', 'Lerem Ipsum Dolor sit amet'),
+  ('Banner 6', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211008/banner_sample_ial2fj.png', 'Lerem Ipsum Dolor sit amet');
+
+INSERT INTO services (service_code, service_name, service_icon, service_tariff)
+VALUES
+  ('PAJAK', 'Pajak PBB', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 40000),
+  ('PLN', 'Listrik', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 10000),
+  ('PDAM', 'PDAM Berlangganan', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 40000),
+  ('PULSA', 'Pulsa', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 40000),
+  ('PGN', 'PGN Berlangganan', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 50000),
+  ('MUSIK', 'Musik Berlangganan', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 50000),
+  ('TV', 'TV Berlangganan', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 50000),
+  ('PAKET_DATA', 'Paket data', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 50000),
+  ('VOUCHER_GAME', 'Voucher Game', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 100000),
+  ('VOUCHER_MAKANAN', 'Voucher Makanan', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 100000),
+  ('QURBAN', 'Qurban', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 200000),
+  ('ZAKAT', 'Zakat', 'https://res.cloudinary.com/diqfczjy9/image/upload/v1732211081/service_sample_knsunb.jpg', 300000);
